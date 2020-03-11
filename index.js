0cc4cd2659a52889a41d596c3c94ef3d1a1da950
Damilola Fagoyinbo <damilola@getchange.com>
2020-03-11 11:19:17 +0200

    Removed bugs again

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const request = require("request");
 console.log("Hello World!");
-request({
-        json: true,
-        headers: {
-          'Authorization': 'token ' + process.env.GITHUB_ACCESS_TOKEN,
-          'User-Agent': 'prMessage-Bot'
-        },
-        method: 'POST',
-        url: pullRequestUrl + '/labels',
-        body: ['bug']
-      }, (error, response, body) => {
-        if (error) {
-          callback(error);
-        } else if (response && response.statusCode && !response.statusCode.toString().startsWith('2')) {
-          callback(new Error(`GitHub API request failed with status ${response.statusCode}`));
-        } else {
-          callback(null, {'message': `added label 'bug' to ${pullRequestUrl}`});
-        }
-      });
+// request({
+//         json: true,
+//         headers: {
+//           'Authorization': 'token ' + process.env.GITHUB_ACCESS_TOKEN,
+//           'User-Agent': 'prMessage-Bot'
+//         },
+//         method: 'POST',
+//         url: pullRequestUrl + '/labels',
+//         body: ['bug']
+//       }, (error, response, body) => {
+//         if (error) {
+//           callback(error);
+//         } else if (response && response.statusCode && !response.statusCode.toString().startsWith('2')) {
+//           callback(new Error(`GitHub API request failed with status ${response.statusCode}`));
+//         } else {
+//           callback(null, {'message': `added label 'bug' to ${pullRequestUrl}`});
+//         }
+//       });
